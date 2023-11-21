92481639bf8ed219e3e80e994ea838240a6c6d3f
Andrew Coblentz <117757138+AndrewCoblentz@users.noreply.github.com>
2023-11-21 16:42:53 -0500

    Update orders.py

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 from .order_details import OrderDetail
 
-
+# This is the foundational class for representing an order. It includes several fields:
 class OrderBase(BaseModel):
     customer_name: str
     description: Optional[str] = None
@@ -12,11 +12,11 @@ class OrderBase(BaseModel):
     type: str
     status: str = "Not Started"
 
-
+#  Inherits from OrderBase and is used for creating new orders.
 class OrderCreate(OrderBase):
     pass
 
-
+# This class is used for updating existing orders. Unlike OrderBase, all its fields are optional.
 class OrderUpdate(BaseModel):
     customer_name: Optional[str] = None
     description: Optional[str] = None
@@ -25,7 +25,7 @@ class OrderUpdate(BaseModel):
     type: Optional[str]
     status: Optional[str] = "In Progress"
 
-
+# This class represents a complete order and includes fields from OrderBase along with additional ones
 class Order(OrderBase):
     id: int
     order_date: Optional[datetime] = None
